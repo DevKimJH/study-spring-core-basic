c55cc3fc3875ae8b41ea2b03b235b9262ff4bdb6
DevKimJH <>
2023-02-01 22:42:02 +0900

    123213

--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -13,7 +13,7 @@ public class OrderServiceTest {
 
     @Test
     void createOrder(){
-        Long memberId = 1L;
+        Long memberId = 2L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
         Order order = orderService.createOrder(memberId, "itemA", 10000);
